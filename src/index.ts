173bff69083b1dd242c70e14a4f5ed4a98e03e70
findQin <find_qin@foxmail.com>
2020-03-02 10:58:09 +0800

    feat 🔥record view elements

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,19 @@
 
 import { UseObserveInit } from './types'
-import useObserve from './useObserve';
+import useObserve from './useObserve'
+import useRecord from './useRecord'
 
 function init(options: UseObserveInit) {
+    const record = useRecord({
+        ignores: (elem) => getComputedStyle(elem).getPropertyValue('visibility') === 'hidden'
+    })
     useObserve({
         observeElem: document.body,
-        onElemAdd: elem => console.log('ADD', elem),
-        onElemRemove: elem => console.log('RMM', elem)
+        onElemAdd: record.recordElemAdd, // elem => console.log('ADD', elem),
+        onElemRemove: record.recordElemRemove // elem => console.log('RMM', elem)
     })
+
+    ;(window as any).record = record
 }
-
-// function observeNode(): void {
-//     const observe = new MutationObserver(mutationCallback)
-//     observe.observe(document.body, {
-//         attributes: false,
-//         childList: true,
-//         subtree: true
-//     })
-
-// }
-
-// function mutationCallback(mutationList: MutationRecord[]): void {
-//     debugger;
-// }
 
 export default init
--- src/useRecord.ts
+++ src/useRecord.ts
@@ -7,8 +7,16 @@ export default function useRecord(options: UseRecordInit): Record {
 
     const aliveElems: HTMLElement[] = []
 
+    /**
+     * 测试待检测元素是否在数组中
+     * @param elem 待检测元素
+     */
     const checkElemInAlive = (elem: HTMLElement) => some(aliveElems, item => item === elem)
 
+    /**
+     * 增加元素时调用方法
+     * @param elem 新增元素
+     */
     const recordElemAdd: ElemChangeCb = (elem: HTMLElement) => {
         // 不满足记录条件
         if (!elem.dataset.vid || ignores(elem)) {
@@ -21,6 +29,10 @@ export default function useRecord(options: UseRecordInit): Record {
         aliveElems.push(elem)
     }
 
+    /**
+     * 删除元素时调用方法
+     * @param elem 删除元素
+     */
     const recordElemRemove: ElemChangeCb = (elem: HTMLElement) => {
         const idx = find(aliveElems, item => item === elem)
         if (idx !== -1) {
